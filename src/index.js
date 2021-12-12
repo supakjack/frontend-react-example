cf4544f0e0f2acb463319d77d6ff0ccc722b8462
DESKTOP-HEK97O0\ADMIN <48548611+supakjack@users.noreply.github.com>
2021-12-12 14:28:27 +0700

    add multipage and components
    add multipage and components

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Home, About, Contact, Blog, Posts, Post } from './pages'
+import {Footer, Navigation } from './components'
+
+ReactDOM.render(
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/blog" element={<Blog />}>
+        <Route path="" element={<Posts />} />
+        <Route path=":postSlug" element={<Post />} />
+      </Route>
+    </Routes>
+    <Footer />
+  </Router>,
+
+  document.getElementById('root')
+)
